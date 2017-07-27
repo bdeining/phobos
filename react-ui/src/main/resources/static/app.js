1f6786366ada79f0f710bbb7487ad059fc195645
bdeining <benjamin.deininger@connexta.com>
2017-07-27 10:33:43 -0600

    React Clean Up

--- react-ui/src/main/resources/static/app.js
+++ react-ui/src/main/resources/static/app.js
@@ -1,12 +1,7 @@
 var App = React.createClass({
-  render() {
-    return ( <EmployeeTable employees={this.state.employees}/> );
-  }
-
-  render() {
-    return ( <h1>Hello World</h1> );
-  }
-
+    render() {
+        return ( <h1>Hello World</h1> );
+    }
 });
 
 ReactDOM.render(<App />, document.getElementById('root') );
